1c1a1cf37b3f746ec4133f011f4b953a8b9669b2
Drew Dahlman <info@drewdahlman.com>
2019-03-12 11:51:22 -0400

    cleanup formatting

--- server/app.js
+++ server/app.js
@@ -11,32 +11,32 @@ let _room = {
 // Connection
 io.on('connection', function (socket) {
 
-// User join
-socket.on("join", () => {
-	io.emit("welcome", {room: _room});
-});
+  // User join
+  socket.on("join", () => {
+  	io.emit("welcome", {room: _room});
+  });
 
-// Register user
-socket.on("register", (data) => {
-	_room.players.push(data);
-	socket.user = data.name;
-	io.emit("welcome", {room: _room});
-});
+  // Register user
+  socket.on("register", (data) => {
+  	_room.players.push(data);
+  	socket.user = data.name;
+  	io.emit("welcome", {room: _room});
+  });
 
-// Update from user
-socket.on('update', function (data) {
-	let _player = _room.players.find( (p) => {
-		return p.name == data.name;
-	});
-	if( _player ){
-		_player.position = data.position;
-		_player.rotation = data.rotation;
-	}
-});
+  // Update from user
+  socket.on('update', function (data) {
+  	let _player = _room.players.find( (p) => {
+  		return p.name == data.name;
+  	});
+  	if( _player ){
+  		_player.position = data.position;
+  		_player.rotation = data.rotation;
+  	}
+  });
 
-// Remove user
-socket.on('disconnect', function () {
-	_room.players = _room.players.filter(function(p) { return p.name != socket.user; });
+  // Remove user
+  socket.on('disconnect', function () {
+  	_room.players = _room.players.filter(function(p) { return p.name != socket.user; });
 		io.emit('user left', {user: {name: socket.user}});
 	});
 });
