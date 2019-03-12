f23efc89cb534b78aa7b763e0c6b1c3c4079e391
Drew Dahlman <info@drewdahlman.com>
2019-03-12 11:49:30 -0400

    updating readme

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
-var server = require('http').Server();
-var io = require('socket.io')(server);
+let server = require('http').Server();
+let io = require('socket.io')(server);
 server.listen(3000);
-
 
 // Base room
 let _room = {
@@ -12,33 +11,33 @@ let _room = {
 // Connection
 io.on('connection', function (socket) {
 
-	// User join
-	socket.on("join", () => {
-	  io.emit("welcome", {room: _room});
+// User join
+socket.on("join", () => {
+	io.emit("welcome", {room: _room});
+});
+
+// Register user
+socket.on("register", (data) => {
+	_room.players.push(data);
+	socket.user = data.name;
+	io.emit("welcome", {room: _room});
+});
+
+// Update from user
+socket.on('update', function (data) {
+	let _player = _room.players.find( (p) => {
+		return p.name == data.name;
 	});
+	if( _player ){
+		_player.position = data.position;
+		_player.rotation = data.rotation;
+	}
+});
 
-	// Register user
-	socket.on("register", (data) => {
-		_room.players.push(data);
-		socket.user = data.name;
-	  io.emit("welcome", {room: _room});
-	});
-
-	// Update from user
-  socket.on('update', function (data) {
-    let _player = _room.players.find( (p) => {
-    	return p.name == data.name;
-    });
-    if( _player ){
-	    _player.position = data.position;
-	    _player.rotation = data.rotation;
-	  }
-  });
-
-  // Remove user
-  socket.on('disconnect', function () {
-    _room.players = _room.players.filter(function(p) { return p.name != socket.user; });
-	  io.emit('user left', {user: {name: socket.user}});
+// Remove user
+socket.on('disconnect', function () {
+	_room.players = _room.players.filter(function(p) { return p.name != socket.user; });
+		io.emit('user left', {user: {name: socket.user}});
 	});
 });
 
--- server/readme.md
+++ server/readme.md
@@ -1,2 +1,9 @@
 # Socket Server
 A simple socket server for a basic multiplayer unity game.
+
+## Running
+- have Docker installed
+- run `docker-compose up`
+
+## Under the hood
+When the server is started it creates a socket listener on port `3000` it also creates an in memory object that represents the "room" which is where players are added as they join and removed as they quit the game.
